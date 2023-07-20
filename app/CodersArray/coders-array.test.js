d1a8b802764a130eb0af3d8674c81d77d955ba0e
Raúl Pérez <jrpcassone@gmail.com>
2023-07-21 01:23:12 +0200

    Implement tests to check numbers and strings conversion

--- app/CodersArray/coders-array.test.js
+++ app/CodersArray/coders-array.test.js
@@ -2,9 +2,20 @@ import CodersArray from "./coders-array.js";
 
 describe("Given a CodersArray class", () => {
   describe("When it receives ''hola', 'mola''", () => {
-    test("Then it should return true", () => {
+    test("Then it should create an object of the instanced class", () => {
       const names = ("hola", "mola");
       const valuesAreAnObject = new CodersArray(names);
+
+      expect(valuesAreAnObject).toBeInstanceOf(CodersArray);
+    });
+  });
+});
+
+describe("Given a CodersArray class", () => {
+  describe("When it receives ''1', 1'", () => {
+    test("Then it should create an object of the instanced class", () => {
+      const numbers = (1, 1);
+      const valuesAreAnObject = new CodersArray(numbers);
 
       expect(valuesAreAnObject).toBeInstanceOf(CodersArray);
     });
